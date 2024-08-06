35058f9421479ef21214f139cb7c9a1d90e45598
zhhsplendid <zhhsplendid@163.com>
2024-08-06 11:02:09 +0800

    Make test formal

--- python/test/single_machine_multi_process_comm_test.py
+++ python/test/single_machine_multi_process_comm_test.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 import torch
 import torch.distributed as dist
@@ -7,40 +8,53 @@ import torch.multiprocessing as mp
 # importing makes torch.distributed recognize `headlm_comm` as a valid backend.
 import headlm_comm
 
-"""Blocking point-to-point communication."""
-def run(rank, size):
-    tensor = torch.ones([2, 3], device="cuda") * rank
 
-    if rank == 0:
-        # Send the tensor to process 1
-        dist.send(tensor=tensor, dst=1)
-    else:
-        # Receive tensor from process 0
-        dist.recv(tensor=tensor, src=0)
-    print(f"Rank {rank} After send/recv, tensor.device = {tensor.device}")
-    print('Rank ', rank, ' has data ', tensor, '\n')
+class TestSingleMachineMultiProcessSendRecv(unittest.TestCase):
+    """Blocking point-to-point communication."""
 
-def init_process(rank, size, fn, backend='headlm'):
-    """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = '127.0.0.1' # 'localhost'
-    os.environ['MASTER_PORT'] = '29512'
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    fn(rank, size)
+    def send_recv_func(self, rank, device):
 
-def main():
-    size = 2
-    processes = []
-    mp.set_start_method("spawn")
-    for rank in range(size):
-        p = mp.Process(target=init_process, args=(rank, size, run))
-        p.start()
-        processes.append(p)
+        tensor = torch.ones([2, 3], device=device) * rank
 
-    for p in processes:
-        p.join()
+        if rank == 0:
+            # Send the tensor to process 1
+            dist.send(tensor=tensor, dst=1)
+        else:
+            # Receive tensor from process 0
+            dist.recv(tensor=tensor, src=0)
+        print(f"Rank {rank} After send/recv, tensor.device = {tensor.device}")
+        print('Rank ', rank, ' has data ', tensor, '\n')
+        self.assertTrue(
+            torch.allclose(torch.zeros([2, 3], device=device), tensor))
+
+    def init_process(self, rank, size, fn, device, backend='headlm'):
+        """ Initialize the distributed environment. """
+        os.environ['MASTER_ADDR'] = '127.0.0.1'  # 'localhost'
+        os.environ['MASTER_PORT'] = '29512'
+        dist.init_process_group(backend, rank=rank, world_size=size)
+        fn(rank, device)
+        dist.destroy_process_group()
+
+    def _test_base(self, device):
+        size = 2
+        processes = []
+        for rank in range(size):
+            class_instance = TestSingleMachineMultiProcessSendRecv()
+            p = mp.Process(target=class_instance.init_process,
+                           args=(rank, size, class_instance.send_recv_func,
+                                 device))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+    def test_cpu(self):
+        self._test_base("cpu")
+
+    def test_cuda(self):
+        self._test_base("cuda")
+
 
 if __name__ == "__main__":
-    #for symbol in dir(headlm_comm):
-    #    print(symbol)
-    #print(headlm_comm.__file__)
-    main()
+    unittest.main()
