624edd026a4444c0364e9a6fbdc22f5c720dd279
JimyMa <hit16s105116@gmail.com>
2025-03-19 02:22:18 +0800

    update slime client

--- Slime/example/slime_client.py
+++ Slime/example/slime_client.py
@@ -5,15 +5,18 @@ import requests
 import torch
 from slime import _slime_c
 
+id = remote_info = requests.get(
+    "http://10.130.8.139:4469/init_link"
+).json()["id"]
 
 ctx = _slime_c.rdma_context()
 
 # Init RDMA
-ctx.init_rdma_context("mlx5_bond_1", 1, "Ethernet")
+ctx.init_rdma_context("mlx5_bond_0", 1, "Ethernet")
 
 # Init Memory Region
 mr_key = "local_kv"
-local_memory = torch.ones([1024, 1024, 1024], device="cuda", dtype=torch.half)* 0.001
+local_memory = torch.ones([1024, 1024, 1024, 16], device="cuda", dtype=torch.int8)
 ctx.register_memory_region(
     mr_key,
     local_memory.data_ptr(),
@@ -27,7 +30,7 @@ local_rdma_info = ctx.get_local_rdma_info()
 local_rdma_info.log()
 
 info = remote_info = requests.get(
-    "http://localhost:4469/get_local_info", 
+    "http://10.130.8.139:4469/get_local_info", json={"id": id}
 ).json()
 
 ctx.modify_qp_to_rtsr(
@@ -39,8 +42,9 @@ ctx.modify_qp_to_rtsr(
 )
 
 requests.post(
-    "http://localhost:4469/exchange_info", 
+    "http://10.130.8.139:4469/exchange_info", 
     json={
+        "id": id,
         "gid": local_rdma_info.get_gid(),
         "gidx": local_rdma_info.gidx,
         "lid": local_rdma_info.lid,
@@ -51,12 +55,25 @@ requests.post(
 )
 
 psum = requests.post(
-    "http://localhost:4469/rdma_read", 
+    "http://10.130.8.139:4469/rdma_read", 
     json={
+        "id":id,
         "remote_rkey": ctx.get_r_key(mr_key),
-        "length": 1024 * 1024 * 512 * local_memory.itemsize,
+        "length": 128 * 64 * 1 * 2 * local_memory.itemsize,
+        "offset": 0,
         "remote_addr": local_memory.data_ptr()
     }
 )
 
 print(psum.json())
+
+psum = requests.post(
+    "http://10.130.8.139:4469/rdma_read", 
+    json={
+        "id": id,
+        "remote_rkey": ctx.get_r_key(mr_key),
+        "length": 1024 * 1024 * 1024 * local_memory.itemsize,
+        "offset": 1024 * 1024 * 512 * local_memory.itemsize,
+        "remote_addr": local_memory.data_ptr()
+    }
+)
