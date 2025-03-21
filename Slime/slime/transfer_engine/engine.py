4eee202191f55e1b782b9b5c41ebe87380425f03
zhhsplendid <zhhsplendid@163.com>
2025-03-21 16:54:50 +0800

    Finish engine send/recv code, but not tested yet

--- Slime/slime/transfer_engine/engine.py
+++ Slime/slime/transfer_engine/engine.py
@@ -1,30 +1,35 @@
+import asyncio
 import torch
 import future
+import zmq
 
 from typing import Dict, Tuple
 
 from .context import RDMAContext
 
-from slime.config import RDMAInfo
+from slime.config import RDMAInfo, ExchangeInfo
 
 
 class TransferEngine:
+
     def __init__(self, dev_name: str, ib_port: int, link_type="Ethernet"):
         self.dev_name = dev_name
         self.ib_port = ib_port
-        self.link_type=link_type
-        self.links:Dict[int, RDMAContext] = {}
+        self.link_type = link_type
+        self.links: Dict[int, RDMAContext] = {}
 
     def init_link(self, session_id: int):
         if session_id in self.links:
             raise KeyError(f"session_id {session_id} already in links")
-        self.links[session_id] = RDMAContext(dev_name=self.dev_name, ib_port=self.ib_port, link_type=self.link_type)
+        self.links[session_id] = RDMAContext(dev_name=self.dev_name,
+                                             ib_port=self.ib_port,
+                                             link_type=self.link_type)
 
     def register_mr(self, session_id, mr_key, length, device="cpu"):
         if session_id not in self.links:
             raise KeyError(f"session_id {id} not in links")
         self.links[session_id].register_mr(mr_key, length, device=device)
-    
+
     def register_torch(self, session_id, mr_key, t):
         if session_id not in self.links:
             raise KeyError(f"session_id {id} not in links")
@@ -34,42 +39,126 @@ class TransferEngine:
         if id not in self.links:
             raise KeyError(f"session_id {id} not in links")
         self.links[id].construct(local_info)
-    
+
     def get_local_info(self, session_id: int) -> RDMAInfo:
         if session_id not in self.links:
             raise KeyError(f"session_id {session_id} not in links")
         local_info = self.links[session_id].get_local_info()
         return local_info
 
-    async def r_rdma_async(self, session_id, mr_key, target_offset, source_offset, length):
+    async def r_rdma_async(self, session_id, mr_key, target_offset,
+                           source_offset, length):
         if session_id not in self.links:
             raise KeyError(f"session_id {session_id} not in links")
-        await self.links[session_id].r_rdma_async(mr_key, target_offset, source_offset, length)
- 
+        await self.links[session_id].r_rdma_async(mr_key, target_offset,
+                                                  source_offset, length)
 
-    async def send_tensor(tensor: torch.Tensor,
-                           send_indices: list[int],
-                           receiver_indices: list[int]
-                           host: str,
-                           port: int): -> future
-        # gather, register mr
-        # tcp receiver send meta
-        
+    async def send_tensor(self, session_id: int, tensor: torch.Tensor,
+                          send_indices: list[int], remote_host: str,
+                          remote_port: int, local_port: int) -> future:
+        """
+        Sender gather tensor into a buffer tensor based on send_indices, then sent rdma infos through tcp to receiver.
+        Receiver can read it after have those info.
+        """
 
+        if session_id not in self.links:
+            raise KeyError(f"session_id {session_id} not in links")
 
-    
-    async def receive_tensors(tensors:torch.Tensor, machine_info): -> future()
-        receive_indices
-        # tcp receive meta
-        # rdma read whole
-        # whole -> scatter -> list[torch.Tensor] -> write
+        #
+        # Gather tensors based on indices
+        #
+        send_index_tensor = torch.Tensor(send_indices)
+        # Reshape and expand the indices to match tensor's dimensions
+        expend_send_index = send_index_tensor.view(
+            -1, *([1] * (tensor.dim() - 1))).expand(-1, *tensor.shape[1:])
+        # Gather the elements along dim=0
+        buffer_tensor = torch.gather(tensor, dim=0, index=expend_send_index)
 
+        #
+        # Regist the gather tensor on MR
+        #
+        rdma_link = self.links[session_id]
+        mr_key = str(buffer_tensor.data_ptr())
+        rdma_link.register_torch(mr_key, buffer_tensor)
 
+        #
+        # Rcp send meta
+        #
+        zmq_ctx = zmq.Context(2)
+        send_socket = zmq_ctx.socket(zmq.PUSH)
+        send_socket.connect(f"tcp://{remote_host}:{remote_port}")
+        recv_socket = zmq_ctx.socket(zmq.PULL)
+        recv_socket.bind(f"tcp://localhost:{local_port}")
 
+        local_rdma_info = rdma_link.get_local_info()
+        local_mr_info = rdma_link.get_mr_info(mr_key)
+        send_socket.send_pyobj([local_rdma_info, local_mr_info])
+        remote_rdma_info, remote_mr_info = recv_socket.recv_pyobj()
+        rdma_link.construct(remote_rdma_info)
 
-    
+    async def receive_tensors(self, session_id: int, out_tensor: torch.Tensor,
+                              buffer_tensor: torch.Tensor,
+                              receiver_indices: list[int], remote_host: str,
+                              remote_port: int, local_port: int) -> future:
+        """
+        Receiver read the remote buffer tensor to local buffer tensor, then scatter it to out_tensor.
+        """
 
-        
+        if session_id not in self.links:
+            raise KeyError(f"session_id {session_id} not in links")
+
+        #
+        # tcp exchange meta
+        #
+        zmq_ctx = zmq.Context(2)
+        send_socket = zmq_ctx.socket(zmq.PUSH)
+        send_socket.connect(f"tcp://{remote_host}:{remote_port}")
+        recv_socket = zmq_ctx.socket(zmq.PULL)
+        recv_socket.bind(f"tcp://localhost:{local_port}")
+
+        remote_rdma_info, remote_mr_info = recv_socket.recv_pyobj()
+
+        #
+        # Regist the buffer tensor on MR
+        #
+        rdma_link = self.links[session_id]
+        mr_key = str(buffer_tensor.data_ptr())
+        rdma_link.register_torch(mr_key, buffer_tensor)
+        rdma_link.register_remote_mr(mr_key, remote_mr_info)
+
+        local_rdma_info = rdma_link.get_local_info()
+        local_mr_info = rdma_link.get_mr_info(mr_key)
+        send_socket.send_pyobj([local_rdma_info, local_mr_info])
+        rdma_link.construct(remote_rdma_info)
+
+        #
+        # Do scatter callback once read finish
+        #
+        loop = asyncio.get_running_loop()
+        future = loop.create_future()
+
+        def _scatter_callback(code):
+            if code == 0:
+                #
+                # Scatter tensors based on indices
+                #
+                receive_index_tensor = torch.Tensor(receiver_indices)
+                # Reshape and expand the indices to match tensor's dimensions
+                expend_receive_index = receive_index_tensor.view(
+                    -1, *([1] * (buffer_tensor.dim() - 1))).expand(
+                        -1, *buffer_tensor.shape[1:])
+                # Scatter the elements along dim=0
+                out_tensor.scatter_(dim=0,
+                                    index=expend_receive_index,
+                                    src=buffer_tensor)
+                # Success, we should run call back
+                loop.call_soon_threadsafe(future.set_result, code)
+            else:
+                loop.call_soon_threadsafe(future.set_exception, code)
+
+        rdma_link.r_rdma_async(mr_key, 0, 0, remote_mr_info.offset,
+                               _scatter_callback)
+
     def stop_link(self, session_id: int):
         if session_id not in self.links:
             raise KeyError(f"session_id {id} not in links")
