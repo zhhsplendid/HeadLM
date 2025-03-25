31a470d752f786e323ef8673bbc44027eb7696c4
zhhsplendid <zhhsplendid@163.com>
2025-03-25 10:49:10 +0800

    Add code for testing bandwidth, sender part

--- Slime/slime/transfer_engine/context.py
+++ Slime/slime/transfer_engine/context.py
@@ -2,7 +2,7 @@ import json
 import time
 import asyncio
 
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 
 import torch
 
@@ -93,5 +93,12 @@ class RDMAContext:
     def get_remote_mr_info(self, mr_key) -> MemoryRegionInfo:
         return self.remote_memory_pool[mr_key]
 
-    def register_remote_mr(self, mr_key, mr_info: MemoryRegionInfo):
-        self.remote_memory_pool[mr_key] = mr_info
+    async def buffered_r_rdma_async(self,
+                                    target_offsets: List[int],
+                                    source_offsets: List[int],
+                                    lengths: List[int],
+                                    remote_host: str,
+                                    remote_port: int,
+                                    local_port: int):
+        pass
+        
